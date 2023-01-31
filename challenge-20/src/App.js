390766be7d02cb5d86d8f392e59ec852ece1ece3
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-01-31 17:21:33 -0600

    Added nav functionality

--- challenge-20/src/App.js
+++ challenge-20/src/App.js
@@ -39,7 +39,7 @@ export default function App() {
           <Contact></Contact>
         )}
       </main>
-      <Portfolio/>
+      {/* <Portfolio/> */}
       <Footer/>
     </div>
 
--- challenge-20/src/components/Toggle.js
+++ challenge-20/src/components/Toggle.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../utils/helpers';
+import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
 
 function Toggle(props) {
   const { currentCategory } = props;
-  return (
-    <section>
-      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
-    </section>
-  );
+
+  if (currentCategory.name === "Resume") {
+    return (
+   <Resume></Resume>
+
+    );
+  }
+  else if (currentCategory.name === "Portfolio") {
+    return (
+<Portfolio></Portfolio>
+    )
+  }
+  else {
+    return (
+<div></div>
+    );
+  }
+
+  // return (
+  //   <section>
+  //     <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+  //     <p>{currentCategory.name}</p>
+  //   </section>
+  // );
 }
+
+
+
 export default Toggle;
